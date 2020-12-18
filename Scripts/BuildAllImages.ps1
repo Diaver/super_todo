700167cf7ef8a2cb343be7a15ed7cc694557abab
Stas <st.glushak@gmail.com>
2020-12-18 00:59:18 -0500

    Fixed BuildAllImages.ps1

--- Scripts/BuildAllImages.ps1
+++ Scripts/BuildAllImages.ps1
@@ -4,3 +4,4 @@ docker build -f ./Users.Api/Dockerfile -t super_todo/users_api:0.1 .
 docker build -f ./TodoTasks.EventHandler/Dockerfile -t super_todo/todotasks_eventhandler:0.1 .
 docker build -f ./Notifications/Dockerfile -t super_todo/notifications:0.1 .
 docker build -f ./Auth.Api/Dockerfile -t super_todo/auth_api:0.1 .
+docker build -f ./Logging.EventHandler/Dockerfile -t super_todo/logging_eventhandler:0.1 .
